d15f0b14eaa33d8e93b1b41df6d0b4bc82187183
Sudarshan Raghunathan <susudars@microsoft.com>
2017-11-08 12:41:15 -0800

    Working version with ability to post messages

--- bclient.py
+++ bclient.py
@@ -8,17 +8,18 @@ HEADERS={'Authorization': 'Bearer ' + ACCESS_KEY}
 
 def _make_request(url, body=None, extra_headers=None, method="POST"):
     with closing(httplib.HTTPSConnection(DIRECTLINE_URL)) as conn:
-        #json_body = body and json.dumps(body)
         if extra_headers:
             headers = HEADERS.copy()
             headers.update(extra_headers)
         else:
             headers = HEADERS
-            
+
         conn.request(method, url, body, headers=headers)
         response = conn.getresponse()
+        print response.status, response.reason
+        r = response.read()
         assert response.status == 200 or response.status == 201
-        return json.loads(response.read())
+        return json.loads(r)
 
 def get_token():
     data = _make_request("/v3/directline/tokens/generate")
@@ -30,23 +31,22 @@ def create_conversation():
 
 def get_activities(converstation_id, index=-1):
     data = _make_request("/v3/directline/conversations/"+converstation_id+"/activities", method="GET")
-    print data
-    return data["activities"][index]["text"]
+    return data["activities"][index] #["text"]
 
 def send_activity(conversation_id, activity):
-    data = _make_request("/v3/directline/conversations/"+conversation_id+"/activities", body=activity)
+    extra_headers = {'Content-Type': "application/json"}
+    data = _make_request("/v3/directline/conversations/"+conversation_id+"/activities", body=activity, extra_headers=extra_headers)
     return data["id"]
 
 def close_converation(conversation_id):
-    return send_activity(conversation_id, json.dumps({'type': 'endOfConversation'}))
+    return send_activity(conversation_id, json.dumps({'type': 'endOfConversation', "from": {"id":"user1"}}))
 
-def upload_image(converstation_id, user_id, file_path):
+def upload_image_file(converstation_id, user_id, file_path):
     import os
-    import base64
     ext = os.path.splitext(file_path)[1][1:]
     mime_type = "image/"+ext
     with open(file_path, "rb") as f:
-        image_data = f.read() #base64.b64encode(f.read())
+        image_data = f.read() 
         
     extra_headers = {'Content-Type': mime_type, 'Context-Disposition': 'name="file"; filename={0}'.format(file_path)}
     data = _make_request("/v3/directline/conversations/{0}/upload?userId={1}".format(converstation_id, user_id),\
@@ -54,3 +54,13 @@ def upload_image(converstation_id, user_id, file_path):
                          extra_headers=extra_headers)
     return data["id"]
 
+def upload_image_url(conversation_id, user_id, url):
+    import os
+    import urlparse
+    import json
+    ext = os.path.splitext(urlparse.urlsplit(url).path)[1][1:]
+    mime_type = "image/"+ext
+    extra_headers = {'Content-Type': "application/json"}
+    activity = {"type":"message", "text": url, "from": {"id": "user1"}, "locale":"en-US", "textFormat":"plain"}
+    data = _make_request("/v3/directline/conversations/{0}/activities".format(conversation_id), body=json.dumps(activity), extra_headers=extra_headers)
+    return data["id"]
